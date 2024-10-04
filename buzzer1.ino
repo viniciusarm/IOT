6b522313ead78476b36e6ce69c4dcb35bb368d45
viniciusarm <vinicius.a.martins@aluno.senai.br>
2024-10-04 08:27:58 -0300

    usando piezzo pela primeira vez

---
+++ buzzer1.ino
@@ -0,0 +1,27 @@
+int buzzer = 3;
+#define doo 262
+#define re 294
+#define mi  330
+#define fa  349
+
+
+
+void setup()
+{
+  pinMode(buzzer, OUTPUT);
+}
+
+void loop()
+{
+ //a funçao de ligar tone()
+  tone(buzzer,doo,500);// tocar o dó
+  delay(600); // espere 600 milisegundos para toca a proxima nota
+  tone(buzzer,re,500);// tocar o re 
+  delay(600);
+  tone(buzzer,mi,500);// tocar o mi 
+  delay(600);
+  tone(buzzer,fa,500);// tocar o fa 
+  delay(600);
+  //a funçao de desligar notone()
+  noTone(buzzer);
+}
